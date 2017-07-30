0a4ea81df1824ed998257fd59d839772a0267b2c
Sneha Inguva <snehainguva@Snehas-MacBook-Pro.local>
2017-07-30 18:44:15 -0400

    changing permissions and cleaning up

--- starter.sh
+++ starter.sh
@@ -1,19 +1,12 @@
 # grab directory
 SCRIPT=`python -c "import os,sys; print(os.path.realpath(os.path.expanduser(sys.argv[1])))" "${BASH_SOURCE:-$0}"`
-DIR=$(dirname $(dirname $SCRIPT))
-
-# make temp compose directory
-sudo mkdir compose
-cd $DIR/compose
+DIR=$(dirname $SCRIPT)
 
 # clone relevant git repo
 git clone https://github.com/openzipkin/docker-zipkin.git
-cd docker-zipkin 
-mv docker-compose.yml $DIR/docker-compose.yml 
-
-# run script to add in our testServer to the compose file
-./update_compose.sh
+cd docker-zipkin
+mv docker-compose.yml $DIR/docker-compose.yml
 
 # cleanup the repo
 cd ..
-rm -rf compose
+rm -rf $DIR/docker-zipkin
